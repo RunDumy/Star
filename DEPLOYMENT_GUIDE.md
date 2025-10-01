5849e45ae2b2e8f2c2e5f7125b32bbd8d009f58f
RunDumy <nglover45@gmail.com>
2025-10-01 06:49:48 -0400

    first commit

---
+++ DEPLOYMENT_GUIDE.md
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:55ec23c98ba2479dc88d113097d5a7ab3be295a79b091f69f0ca51e7a7b467db
+size 7744
---
+++ install.sh
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:979aead79541949c0cb1e0d25467473a2989a6743d5aa90307668b66f375a9d0
+size 3354
